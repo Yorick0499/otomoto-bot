a53f072c8baf6a6bdcefa694c2b62e2110901033
Kacper Rzońca <kacper11812@gmail.com>
2025-11-07 19:19:53 +0100

    chore: update debug messages in languages.py

--- lang/languages.py
+++ lang/languages.py
@@ -3,22 +3,24 @@ translations = {
         "user_input_brand": "Enter car's brand: ",
         "user_input_model": "Enter model: ",
         "enter": "Entering website...",
-        "loading": "Loading webiste...",
+        "simulating": "Simulating user actions...",
+        "fetching": "Fetching data...",
         "parsing": "Parsing data...",
         "extract": "Extracting data...",
         "cleanup": "Cleanup...",
         "new": "Detected new offers!",
-        "done": "Done."
+        "none": "No new offers."
     },
     "pl": {
         "user_input_brand": "Wpisz markę samochodu: ",
         "user_input_model": "Wpisz model samochodu: ",
         "enter": "Uzyskiwanie dostępu...",
-        "loading": "Ładowanie witryny...",
+        "simulating": "Symulowanie akcji użytkownika...",
+        "fetching": "Pobieranie danych...",
         "parsing": "Przetwarzanie danych...",
         "extract": "Ekstrakcja danych...",
         "cleanup": "Czyszczenie sesji...",
         "new": "Znaleziono nowe oferty!",
-        "done": "Zrobione."
+        "none": "Brak nowych ofert."
     }
 }
